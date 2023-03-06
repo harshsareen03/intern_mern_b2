7180edb3958b637fabc6e75a8911d9cb90a91121
harshsareen03 <harshsareen03@gmail.com>
2023-03-06 16:17:30 +0530

    js_ifelse

---
+++ session_03/conditon.js
@@ -0,0 +1,16 @@
+let a=35
+let b=35
+if(a>b){
+    console.log('a is greater than b')
+}
+else if (a<b){
+    console.log('a is less than b')
+}
+
+else{
+    console.log('a is euals to b')
+}
+
+let c=23
+
+c>a?console.log('i dont know'):console.log('c is euals to a')
