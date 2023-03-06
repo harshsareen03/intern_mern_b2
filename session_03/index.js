10ec3ff9a02d703165be63227052e753e8bebf78
harshsareen03 <harshsareen03@gmail.com>
2023-03-06 16:08:35 +0530

    js_1

---
+++ session_03/index.js
@@ -0,0 +1,103 @@
+// console.log('hello')
+// console.assert(45>50)
+// alert('hello')
+// console.error('i dont know')
+// prompt('hello i am prompt')
+// console.table([1,2,3])
+
+// there are two types of variable 
+
+// 1)blocking variable 
+//  let and const
+// 2) non-blocking variable
+// var
+// data types
+
+// i)primitive data type
+// const a='hello'
+// console.log(a)
+// console.log(a,'a')
+
+// {
+//     const a=35
+//     console.log(a)
+
+// }
+// console.log(a)
+
+
+console.log('data type')
+
+
+// 1)string
+const a='hello'
+
+console.log(typeof a ,a)
+
+// ,2)Number
+const b=25
+console.log(typeof b ,b)
+
+// ,3)boolean,
+const c=true
+console.log(typeof c ,c)
+
+// 4)null
+const d=null
+console.log(typeof d ,d)
+
+// 5)undefined
+let k
+k=true
+console.log(typeof k,k)
+
+// 2)refrence data type
+
+// const f=function(){
+//     return 'hi'
+
+
+// }
+
+// let y=f()
+// console.log(typeof f(),y)
+
+
+
+// const ch={
+//     a:'b',
+//     c:'d'
+// }
+
+// console.log(typeof ch,ch)
+
+
+// functions 
+
+function name(a,b){
+
+    return a+b
+}
+
+let jk=name(30,50)
+console.log(typeof jk)
+
+
+const kl=function(b,c){
+    return b-c
+}
+
+let lp=kl(70,60)
+console.log(lp)
+
+let r=(m,n)=>{
+    return m*n
+
+}
+
+const lo=r(50,60)
+console.log(lo)
+
+
+
+
