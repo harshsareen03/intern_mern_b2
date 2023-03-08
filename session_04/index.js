e54ca7c19c32ffc3b57b049dabb98d296efeed2c
harshsareen03 <harshsareen03@gmail.com>
2023-03-08 16:37:18 +0530

    loop

---
+++ session_04/index.js
@@ -0,0 +1,39 @@
+
+
+for (let i = 0; i < 10; i++) {
+    console.log('hi')
+
+}
+
+let sum = 0
+const numbers = [65, 44, 12, 30]
+// numbers.forEach(
+//     function myfunction(item) {
+//         sum += item
+//         console.log(sum)
+//     }
+
+// )
+
+
+numbers.forEach(
+    myfunction = (item) => {
+        sum += item
+        console.log(sum)
+    }
+
+)
+const l = 10
+
+console.log(`hello ${l}`)
+// array.forEach(element => { a=()=>{
+
+// }
+
+// });
+
+// console.log('hi')/
+
+
+// const arr = numbers.map(Math.sqrt)
+// console.log(arr)
